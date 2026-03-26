8a20861902355691963db43582f05fccd9db298c
Abhinav Gyawali <104615184+abhinav0014@users.noreply.github.com>
2026-03-26 09:22:57 +0545

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,4 +27,6 @@ dependencies {
     implementation(files("libs/DM-UPnP-Android-1.4.9-STABLE.jar"))
 
     implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation "com.google.android.material:material:1.10.0" // or latest stable
+    implementation "androidx.core:core-splashscreen:1.0.1" // for SplashScreen API
 }
