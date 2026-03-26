84ec97773761ccb50933c1f545e618074f7d3c87
Abhinav Gyawali <104615184+abhinav0014@users.noreply.github.com>
2026-03-26 08:56:21 +0545

    Update settings.gradle.kt

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,9 @@
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven { url = uri("https://dl.google.com/dl/android/maven2/") }
-        maven { url = uri("https://repo1.maven.org/maven2/") }
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
     }
 }
 
